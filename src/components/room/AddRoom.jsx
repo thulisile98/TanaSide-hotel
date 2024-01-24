f9fab82382cb170f7b8312a06801940f2ba6ef21
Angel Mashego <thulisile.ma98@gmail.com>
2024-01-24 14:46:10 +0200

    made changes to my addroom and  roomtype component

--- src/components/room/AddRoom.jsx
+++ src/components/room/AddRoom.jsx
@@ -1,123 +1,132 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
+import { addRoom } from "../utils/ApiFunctions"
+import RoomTypeSelector from "../common/RoomTypeSelector"
+import { Link } from "react-router-dom"
 
-import {addRoom} from "../utils/ApiFunctions"
+const AddRoom = () => {
+	const [newRoom, setNewRoom] = useState({
+		photo: null,
+		roomType: "",
+		roomPrice: ""
+	})
 
-function AddRoom() {
+	const [successMessage, setSuccessMessage] = useState("")
+	const [errorMessage, setErrorMessage] = useState("")
+	const [imagePreview, setImagePreview] = useState("")
 
-    const [newRoom, setNewRoom]= useState({
-        photo:null,
-        roomType: "",
-        roomPrice: ""
-    })
+	const handleRoomInputChange = (e) => {
+		const name = e.target.name
+		let value = e.target.value
+		if (name === "roomPrice") {
+			if (!isNaN(value)) {
+				value = parseInt(value)
+			} else {
+				value = ""
+			}
+		}
+		setNewRoom({ ...newRoom, [name]: value })
+	}
 
+	const handleImageChange = (e) => {
+		const selectedImage = e.target.files[0]
+		setNewRoom({ ...newRoom, photo: selectedImage })
+		setImagePreview(URL.createObjectURL(selectedImage))
+	}
 
-    const [imagePreview, setImagePreview] = useState("")
-    const[successMessage, setSuccessMessage] = useState("")
-    const[errorMessage, setErrorMessage] = useState("")
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+			if (success !== undefined) {
+				setSuccessMessage("A new room was  added successfully !")
+				setNewRoom({ photo: null, roomType: "", roomPrice: "" })
+				setImagePreview("")
+				setErrorMessage("")
+			} else {
+				setErrorMessage("Error adding new room")
+			}
+		} catch (error) {
+			setErrorMessage(error.message)
+		}
+		setTimeout(() => {
+			setSuccessMessage("")
+			setErrorMessage("")
+		}, 3000)
+	}
 
-    const handleRoomInputChange =(e)=>{
-        const name = e.target.name
-        let value = e.target.value
-        if(name === "roomPrice"){
-            if(!isNaN(value))
-            value.parseInt(value)
-        }else
-        {
-            value = ""
-        }
+	return (
+		<>
+			<section className="container mt-5 mb-5">
+				<div className="row justify-content-center">
+					<div className="col-md-8 col-lg-6">
+						<h2 className="mt-5 mb-2">Add a New Room</h2>
+						{successMessage && (
+							<div className="alert alert-success fade show"> {successMessage}</div>
+						)}
 
-        setNewRoom({...newRoom, [name]: value })
+						{errorMessage && <div className="alert alert-danger fade show"> {errorMessage}</div>}
 
-    }
+						<form onSubmit={handleSubmit}>
+							<div className="mb-3">
+								<label htmlFor="roomType" className="form-label">
+									Room Type
+								</label>
+								<div>
+									<RoomTypeSelector
+										handleRoomInputChange={handleRoomInputChange}
+										newRoom={newRoom}
+									/>
+								</div>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="roomPrice" className="form-label">
+									Room Price
+								</label>
+								<input
+									required
+									type="number"
+									className="form-control"
+									id="roomPrice"
+									name="roomPrice"
+									value={newRoom.roomPrice}
+									onChange={handleRoomInputChange}
+								/>
+							</div>
 
-    const handleImageChange=(e)=>{
-        const selectedImage = e.target.files[0]
-        setNewRoom({...newRoom, photo: selectedImage})
-        setImagePreview(URL.createObjectURL(selectedImage))
-
-    }
-
-    const handleSubmit= async(e) =>{
-        e.prventDefault()
-        try{
-            const success = await addRoom(newRoom.photo, newRoom.roomType , newRoom.roomPrice)
-            if(success !==undefined){
-                setSuccessMessage("A new room was added ")
-                setNewRoom({photo:null, roomType:"",roomPrice:""})
-                setImagePreview("")
-                setErrorMessage("")
-            }else{
-                setErrorMessage("Error adding room")
-            }
-
-        }catch(error){
-            setErrorMessage(error.message)
-        }
-    }
-
-
-  return (
-    <>
-    <section className="container, mt-5 mb-5">
-        <div className="row justify-content-center">
-            <div className="col-md-8 col-lg-6">
-                <h2 className="mt=5 mb-2">Add a New Room</h2>
-
-                <form onSubmit={handleSubmit}>
-                    <div className="mb3">
-                        <label htmlFor="roomType" className="form-label">
-                            Room Type
-                        </label>
-                        <div></div>
-                    </div>
-
-                    <div className="mb3">
-                        <label htmlFor="roomPrice" className="form-label">
-                            Room Price
-                        </label>
-                        <input  className="form-control"  
-                         id="roomPrice" 
-                         name="roomPrice"
-                          value={newRoom.roomPrice} 
-                          type="number" 
-                          onChange={handleRoomInputChange} 
-                        
-                          />
-                        <div></div>
-                    </div>
-
-                    <div className="mb3">
-                        <label htmlFor="photo" className="form-label">
-                            Room Photo
-                        </label>
-                        <input  className="form-control"  
-                         id="photo" 
-                         name="photo"
-                          value={newRoom.photo}  
-                          type="file"
-                          onChange={handleImageChange} 
-                          />
-                          {imagePreview && (<img src={imagePreview} alt="Preview Room Photo"
-                          style={{maxWidth: "400px", maxHeight: "400px"}}
-                          className="mb-3" />
-                          )}
-                    
-                    </div>
-
-                     <div className="d-grid d-md-flex mt-2">
-                            <button className="btn btn-outline-primary ml-5">
-                                Save Room
-                            </button>
-
-                     </div>
-                </form>
-            </div>
-        </div>
-
-    </section>
-    
-    </>
-  )
+							<div className="mb-3">
+								<label htmlFor="photo" className="form-label">
+									Room Photo
+								</label>
+								<input
+									required
+									name="photo"
+									id="photo"
+									type="file"
+									className="form-control"
+									onChange={handleImageChange}
+								/>
+								{imagePreview && (
+									<img
+										src={imagePreview}
+										alt="Preview  room photo"
+										style={{ maxWidth: "400px", maxHeight: "400px" }}
+										className="mb-3"></img>
+								)}
+							</div>
+							<div className="d-grid gap-2 d-md-flex mt-2">
+								{/* <Link to={"/existing-rooms"} className="btn btn-outline-info">
+									Existing rooms
+								</Link> */}
+								<button type="submit" className="btn btn-outline-primary ml-5">
+									Save Room
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
 
 export default AddRoom
